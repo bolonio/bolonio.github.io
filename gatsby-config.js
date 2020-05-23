dee6d31702614e0c30664e97b24458f8e7e87fe1
Adrián Bolonio <adrian.bolonio@willhaben.at>
2020-05-24 00:17:07 +0200

    fix social links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Adrián Bolonio - Frontend Web Developer & Web Accessibility (a11y) advocate`,
+    title: `Adrián Bolonio | Frontend Web Developer & Web Accessibility (a11y) advocate`,
     author: `Adrián Bolonio`,
     bio: `Hi, my name is Adrián Bolonio. I’m an experienced Team Lead, Frontend Developer, and Web Accessibility (a11y) advocate. I'm from Spain, but since 2012 I've been living and working in Vienna (Austria). When I’m not at the office I enjoy a good read, working my way through any delicious recipe, and indulging my love for travelling to new places.`,
     description: `I’m an experienced Team Lead, Frontend Developer, and Web Accessibility (a11y) advocate.`,
@@ -9,14 +9,12 @@ module.exports = {
     language: "en",
     twitterUser: "@bolonio",
     image: "/images/adrianbolonio.jpg",
-    social: [
-      {
-        twitter: `https://www.twitter.com/bolonio`,
-        github: `https://www.github.com/bolonio`,
-        linkedin: `https://www.linkedin.com/in/adrianbolonio`,
-        instagram: `https://www.instagram.com/bolonio`,
-      },
-    ],
+    social: {
+      twitter: `https://www.twitter.com/bolonio`,
+      github: `https://www.github.com/bolonio`,
+      linkedin: `https://www.linkedin.com/in/adrianbolonio`,
+      instagram: `https://www.instagram.com/bolonio`,
+    },
     navigation: [
       {
         title: `Home`,
